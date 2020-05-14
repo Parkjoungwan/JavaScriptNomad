22d5d4f9900312b59924e5df68ed60cc409b9921
parkjoungwan <whddhs6645@gmail.com>
2020-05-15 00:39:46 +0900

    if-else function practice

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const age = prompt("How old are you");
+const title = document.querySelector("#title");
 
-if (age >10){
-    console.log("you can drink")
-}else{
-    console.log("you cant")
+const BASE_COLOR = "rgb(52, 73, 74)";
+const OTHER_COLOR = "#fffff0";
+
+function handleClick() {
+    const currentColor = title.style.color;
+    if (currentColor === BASE_COLOR){
+        title.style.color = OTHER_COLOR;
+    } else {
+        title.style.color = BASE_COLOR;
+    }
 }
+
+
+function init() {
+    title.style.color = BASE_COLOR;
+    title.addEventListener("mouseenter", handleClick);
+}
+
+init();
+
+
+function handleOffline() {
+    console.log("Bye my friend.");
+}
+
+function handleOnline() {
+    console.log("Welcome back, my friend.");
+}
+
+window.addEventListener("offline", handleOffline);
+window.addEventListener("online", handleOnline);
