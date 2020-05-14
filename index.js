f57767418cc2594fbb59066ecc946ab655226df3
parkjoungwan <whddhs6645@gmail.com>
2020-05-14 23:18:14 +0900

    modifiying the dom with js

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-const title = document.getElementById("title")
+const title = document.querySelector("#title")
 title.innerHTML = "Hi i'm park"
-console.log(title);
-
-console.error("Fuck")
-//DOM: document object model
-//자바스크립는 html의 모든 요소를 객체로 만들어와 가져온다.->document
+console.dir(title);
+title.style.color = "red";
+document.title = "i own you now"
