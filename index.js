50ee65bfc723da2c18007c66fd3b406fedca5ebe
joungwanpark <whddhs6645@gmail.com>
2020-05-15 15:11:29 +0900

    saving username part two

--- index.js
+++ index.js
@@ -1,33 +1,11 @@
 const title = document.querySelector("#title");
+const CLICKED_CLASS = "clicked"
 
-const BASE_COLOR = "rgb(52, 73, 74)";
-const OTHER_COLOR = "#fffff0";
-
-function handleClick() {
-    const currentColor = title.style.color;
-    if (currentColor === BASE_COLOR){
-        title.style.color = OTHER_COLOR;
-    } else {
-        title.style.color = BASE_COLOR;
-    }
+function handleClick(){
+    title.classList.toggle(CLICKED_CLASS)
 }
-
 
 function init() {
-    title.style.color = BASE_COLOR;
-    title.addEventListener("mouseenter", handleClick);
+    title.addEventListener("click", handleClick);
 }
-
 init();
-
-
-function handleOffline() {
-    console.log("Bye my friend.");
-}
-
-function handleOnline() {
-    console.log("Welcome back, my friend.");
-}
-
-window.addEventListener("offline", handleOffline);
-window.addEventListener("online", handleOnline);
