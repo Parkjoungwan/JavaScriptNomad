a2b9b413d648c4f1a4f9726c82adeba034626c9a
parkjoungwan <whddhs6645@gmail.com>
2020-05-16 01:56:47 +0900

    todolist

--- gretting.js
+++ gretting.js
@@ -2,27 +2,32 @@ const form = document.querySelector(".js-form")
 const input = document.querySelector("input")
 const greeting = document.querySelector(".js-greetings")
 
-const USER_LS = "currentUser",
-    SHOWING_CN = "showing";
-function askForName(){
-    form.classList.add(SHOWING_CN)
-    form.addEventListener("submit", handleSubmit)
-}
-function savaName(text){
-    localStorage.setItem(USER_LS, text)
-}
-
+const USER_LS = "currentUser",SHOWING_CN = "showing";
+//이벤드 핸들러 - 입력받아서 currentValue에 넣어줌(이름)
 function handleSubmit(event){
     event.preventDefault();
     const currentValue =  input.value
     paintGreeting(currentValue)
+    saveName(currentValue)
 
 }
+//이름을 물어보자.
+function askForName(){
+    form.classList.add(SHOWING_CN)
+    form.addEventListener("submit", handleSubmit)
+}
+//이름을 저장
+function saveName(text){
+    localStorage.setItem(USER_LS, text)
+}
+
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN)
     greeting.classList.add(SHOWING_CN)
     greeting.innerText = `Hello ${text}`
 }
+
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS)
     if(currentUser===null){
@@ -30,7 +35,6 @@ function loadName(){
     }else{
         paintGreeting(currentUser)
     }
-    console.log(currentUser)
 }
 function init(){
     loadName()
